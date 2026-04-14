aa0fc3dcfd5c8b1322c6008a1ff0f574a7425d69
yaoshikuro <yukivoron@gmail.com>
2026-04-14 16:20:15 +0300

    Step 10: browser console basics and simple math

--- main.js
+++ main.js
@@ -94,3 +94,11 @@ console.log(obj1 == obj3);
 let arr1 = [1, 2, 3];
 let arr2 = [1, 2, 3];
 console.log(arr1 == arr2);
+
+let x1 = 10;
+let y1 = 5;
+
+console.log(x1 + y1);
+console.log(x1 - y1);
+console.log(x1 * y1);
+console.log(x1 / y1);
