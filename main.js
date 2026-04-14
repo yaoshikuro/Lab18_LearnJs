388e57323533014e6cd421f43af3e1d786408d52
yaoshikuro <yukivoron@gmail.com>
2026-04-14 16:13:58 +0300

    Step 9: strict and loose comparison

--- main.js
+++ main.js
@@ -75,3 +75,22 @@ console.log(null + 1);
 console.log(undefined + 1);
 
 console.log("5" + 3 + 2);
+
+console.log(5 == "5");
+console.log(5 === "5");
+console.log(0 == false);
+console.log(0 === false);
+console.log(null == undefined);
+console.log(null === undefined);
+
+let obj1 = { name: "John" };
+let obj2 = { name: "John" };
+console.log(obj1 == obj2);
+console.log(obj1 === obj2);
+
+let obj3 = obj1;
+console.log(obj1 == obj3);
+
+let arr1 = [1, 2, 3];
+let arr2 = [1, 2, 3];
+console.log(arr1 == arr2);
