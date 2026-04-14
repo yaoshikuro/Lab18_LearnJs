10c04bce25803a2cec2b5a6ddcdc7b726f285e66
yaoshikuro <yukivoron@gmail.com>
2026-04-14 16:07:32 +0300

    Step 6: variables and data types in JavaScript

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-console.log("Hello, JavaScript!");
+let age = 20;
+let name = "Denis";
+let isStudent = true;
+console.log("Name: ", name);
+console.log("Age: ", age);
+console.log("Is student: ", isStudent);
+
+let value = 10;
+console.log(value);
+value = "Теперь это строка";
+console.log(value);
+value = true;
+console.log(value);
+
+let userName = "Алексей";
+console.log(`Привет, ${userName}!`);
+
+let price = 99.99;
+let temperature = -15;
+console.log(0.1 + 0.2);
+
+let isAlive = true;
+let x;
+console.log(x);
+
+let userData = null;
+console.log(userData);
+
+const PI = 3.14;
+console.log(PI);
+
+let a = 10;
+let b = 3;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(10 + "5");
+console.log("10" - 5);
